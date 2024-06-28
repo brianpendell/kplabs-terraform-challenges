4b53ec5945ffd5da60462da8f403e68849142e3a
bpendell <brian.pendell@cengage.com>
2024-06-28 16:52:14 -0400

    Step 1:  Install and run without changes.

--- challenge-1/tf-challenge-1.tf
+++ challenge-1/tf-challenge-1.tf
@@ -1,17 +1,25 @@
+#this did not work with the version of terraform installed by default. I had to 
+# uninstall terraform and install the older version thus: 
+#Installed old version of terraform thus: 
+#sudo yum remove terraform 
+#wget https://releases.hashicorp.com/terraform/0.12.31/terraform_0.12.31_linux_amd64.zip
+# this file, once unzipped, was the actual terraform binary which I then 
+# had to add to my path so that it would function as expected.  Although
+# in this case I just called it directly as ..\terraform, since it was 
+# in the directory above my terraform work directory. 
+
 provider "aws" {
-  version = "~> 2.54"
-  region  = "us-east-1"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+version = "~> 2.54"
+region = "us-east-1"
 }
 
 provider "digitalocean" {}
 
 terraform {
-    required_version = "0.12.31"
+ required_version = "0.12.31"
 }
 
 
 resource "aws_eip" "kplabs_app_ip" {
-  vpc      = true
+ vpc  = true
 }
