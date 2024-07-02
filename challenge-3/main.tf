d0eea7ac5ae1f00b72f738f2f728a06475514a83
bpendell <brian.pendell@cengage.com>
2024-07-02 18:26:41 -0400

    refactored to standard format even if they didn't ask for it.

--- challenge-3/main.tf
+++ challenge-3/main.tf
@@ -1,13 +1,5 @@
 
-variable "instance_config" {
-  type = map
-  default = {
-    instance1 = { instance_type = "t2.micro", ami = "ami-03a6eaae9938c858c" }
-    instance2 = { instance_type = "t2.small", ami = "ami-053b0d53c279acc90" }
-  }
-}
-
-resource  "aws_instance" "first_ec2" {
+resource  "aws_instance" "ec2_iterate" {
   for_each = var.instance_config 
    tags = {
     Name = each.key
